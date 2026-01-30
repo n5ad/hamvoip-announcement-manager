4fcfafa5187b1bcb3a25c75d63fd87633fd3d136
n5ad <n5ad@att.net>
2026-01-29 18:19:24 -0600

    Update list_mp3.php

--- list_mp3.php
+++ list_mp3.php
@@ -1,51 +1,30 @@
 <?php
-
 /**
-
  * list_mp3.php
-
  * Lists both .mp3 and .wav files in /mp3/ for the Announcements Manager
-
- * CREATED BY N5AD
-
+ * CREATED BY N5AD – adapted for Allmon3
  */
-
 
 $files = [];
 
-
 // Find .mp3 files
-
 $mp3_files = glob("/mp3/*.mp3");
-
 foreach ($mp3_files as $f) {
-
     $files[] = basename($f);
-
 }
-
 
 // Find .wav files
-
 $wav_files = glob("/mp3/*.wav");
-
 foreach ($wav_files as $f) {
-
     $files[] = basename($f);
-
 }
-// Sort alphabetically for nicer display
 
+// Sort alphabetically
 sort($files);
 
-
-// Remove duplicates (in case someone has both .mp3 and .wav with same base name)
-
+// Remove duplicates (same base name .mp3 + .wav)
 $files = array_unique($files);
 
-
 header('Content-Type: application/json');
-
 echo json_encode($files);
-
 ?>
